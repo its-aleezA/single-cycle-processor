6188942649a6c1a1f4f7667c676d89e25b9dfffa
Aleeza Rizwan <aleeza.rizwan.713@gmail.com>
2025-08-12 21:48:57 +0500

    Create Data_Memory.v

---
+++ code/Data_Memory.v
@@ -0,0 +1,19 @@
+//data memory module
+module Data_Memory(clk, rst, WE, WD, A, RD);
+    input clk, rst, WE;
+    input [31:0] A, WD;
+    output [31:0] RD;
+    
+    reg [31:0] mem [0:1023];
+    
+    always @(posedge clk) begin
+        if (WE)
+            mem[A[11:2]] <= WD;
+    end
+    
+    assign RD = (!rst) ? 32'd0 : mem[A[11:2]];
+    
+    initial begin
+        mem[0] = 32'h00000000;
+    end
+endmodule
